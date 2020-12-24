363b861b1d4a055babe3c43f96ca545a940a0d01
Four D. Thirty <45021583+4d30@users.noreply.github.com>
2020-12-24 11:55:38 +0000

    added project files

---
+++ diner/internals.pl
@@ -0,0 +1,54 @@
+# LaTeX2HTML 2016 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/fig:knn/;
+$ref_files{$key} = "$dir".q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/conclusions/;
+$ref_files{$key} = "$dir".q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:citymap/;
+$ref_files{$key} = "$dir".q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:hipster_heatmap/;
+$ref_files{$key} = "$dir".q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/documentclasses/;
+$ref_files{$key} = "$dir".q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:hood146/;
+$ref_files{$key} = "$dir".q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:cluster/;
+$ref_files{$key} = "$dir".q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:venues_map/;
+$ref_files{$key} = "$dir".q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:comp_heatmap/;
+$ref_files{$key} = "$dir".q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:csmap/;
+$ref_files{$key} = "$dir".q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:scoremap/;
+$ref_files{$key} = "$dir".q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:zscores/;
+$ref_files{$key} = "$dir".q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ diner/labels.pl
@@ -0,0 +1,109 @@
+# LaTeX2HTML 2016 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/fig:csmap/;
+$external_labels{$key} = "$URL/" . q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:comp_heatmap/;
+$external_labels{$key} = "$URL/" . q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:venues_map/;
+$external_labels{$key} = "$URL/" . q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:cluster/;
+$external_labels{$key} = "$URL/" . q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:zscores/;
+$external_labels{$key} = "$URL/" . q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:scoremap/;
+$external_labels{$key} = "$URL/" . q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:hipster_heatmap/;
+$external_labels{$key} = "$URL/" . q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:knn/;
+$external_labels{$key} = "$URL/" . q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/conclusions/;
+$external_labels{$key} = "$URL/" . q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:citymap/;
+$external_labels{$key} = "$URL/" . q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/documentclasses/;
+$external_labels{$key} = "$URL/" . q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:hood146/;
+$external_labels{$key} = "$URL/" . q|diner.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2016 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/fig:comp_heatmap/;
+$external_latex_labels{$key} = q|5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:csmap/;
+$external_latex_labels{$key} = q|10|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:cluster/;
+$external_latex_labels{$key} = q|8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:venues_map/;
+$external_latex_labels{$key} = q|3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:scoremap/;
+$external_latex_labels{$key} = q|9|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:zscores/;
+$external_latex_labels{$key} = q|6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:hipster_heatmap/;
+$external_latex_labels{$key} = q|4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:citymap/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/conclusions/;
+$external_latex_labels{$key} = q|6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:knn/;
+$external_latex_labels{$key} = q|7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:hood146/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/documentclasses/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+1;
+
